4ddb41aea1af8a38d920ae3bd7c3a34e5d6a04f9
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-04-21 21:52:51 +0200

    Fix IsTrait

--- src/Resources/templates/Trait/IsTrait.php
+++ src/Resources/templates/Trait/IsTrait.php
@@ -3,20 +3,16 @@ declare(strict_types=1);
 
 namespace App\Trait;
 
-use Webmozart\Assert\Assert;
-
 trait IsTrait
 {
 	public function is(self $object): bool
 	{
-		Assert::propertyExists($object, 'id', 'Object must have an id property');
 		return $this === $object;
 	}
 
 
 	public function isNot(self $object): bool
 	{
-		Assert::propertyExists($object, 'id', 'Object must have an id property');
-		return $this !== $object;
+		return !$this->is($object);
 	}
 }
