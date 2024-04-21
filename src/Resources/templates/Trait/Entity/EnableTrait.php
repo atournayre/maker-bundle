253c07f560c661746821d0f40cd3060bc9c8d745
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-04-21 21:45:48 +0200

    Add EnableTrait

---
+++ src/Resources/templates/Trait/Entity/EnableTrait.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait EnableTrait
+{
+    #[ORM\Column]
+    private ?bool $enable = false;
+
+    public function isEnabled(): ?bool
+    {
+        return $this->enable;
+    }
+
+    public function setEnable(bool $enable): self
+    {
+        $this->enable = $enable;
+
+        return $this;
+    }
+
+    public function enable(): void
+    {
+        $this->enable = true;
+    }
+
+    public function disable(): void
+    {
+        $this->enable = false;
+    }
+}
