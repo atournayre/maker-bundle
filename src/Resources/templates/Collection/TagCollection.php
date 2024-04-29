f1f876814a1e9a2f2ef6cd38a835c7a7532dcd3b
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-04-29 07:35:11 +0200

    Create TagCollection

---
+++ src/Resources/templates/Collection/TagCollection.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Collection;
+
+use Atournayre\Collection\TypedCollection;
+use Webmozart\Assert\Assert;
+
+final class TagCollection extends TypedCollection
+{
+    private const TAG_MIN_LENGTH = 3;
+    private const TAG_MAX_LENGTH = 5;
+
+    public function validateElement(mixed $value): void
+    {
+        Assert::lengthBetween(
+            $value,
+            self::TAG_MIN_LENGTH,
+            self::TAG_MAX_LENGTH,
+            sprintf('Tag "%s" length must be between %d and %d characters', $value, self::TAG_MIN_LENGTH, self::TAG_MAX_LENGTH)
+        );
+    }
+}
