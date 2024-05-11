5485ce7d033d0b7344ab3f0f4a5da8ab291177ef
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-05-11 11:29:58 +0200

    Test : add interface tests (+ dataprovider)

--- tests/Builder/InterfaceBuilderTest.php
+++ tests/Builder/InterfaceBuilderTest.php
@@ -9,27 +9,57 @@ use Atournayre\Bundle\MakerBundle\Printer\PhpFilePrinter;
 
 class InterfaceBuilderTest extends BuilderTestCase
 {
+    public static function dataProvider(): array
+    {
+        return [
+            'Interface with suffix' => [
+                'make-interface/DummyInterface.php',
+                'App\Contracts',
+                'DummyInterface',
+                InterfaceMakerConfiguration::fromNamespace(
+                    rootDir: __DIR__,
+                    rootNamespace: self::rootNamespace(),
+                    namespace: 'App\Contracts',
+                    className: 'DummyInterface',
+                )
+            ],
+            'Interface without suffix' => [
+                'make-interface/DummyInterface.php',
+                'App\Contracts',
+                'Dummy',
+                InterfaceMakerConfiguration::fromNamespace(
+                    rootDir: __DIR__,
+                    rootNamespace: self::rootNamespace(),
+                    namespace: 'App\Contracts',
+                    className: 'Dummy',
+                )
+            ],
+            'Interface without suffix lower case' => [
+                'make-interface/DummyInterface.php',
+                'App\Contracts',
+                'dummy',
+                InterfaceMakerConfiguration::fromNamespace(
+                    rootDir: __DIR__,
+                    rootNamespace: self::rootNamespace(),
+                    namespace: 'App\Contracts',
+                    className: 'dummy',
+                )
+            ],
+        ];
+    }
+
     /**
-     * @return void
-     * @throws \Throwable
+     * @dataProvider dataProvider
      * @covers \Atournayre\Bundle\MakerBundle\Builder\InterfaceBuilder::createPhpFileDefinition
      * @covers \Atournayre\Bundle\MakerBundle\Builder\InterfaceBuilder::supports
      * @covers \Atournayre\Bundle\MakerBundle\Builder\AbstractBuilder::createPhpFileDefinition
      * @covers \Atournayre\Bundle\MakerBundle\Config\InterfaceMakerConfiguration::fromNamespace
      * @covers \Atournayre\Bundle\MakerBundle\Printer\PhpFilePrinter::print
      */
-    public function testBuildInterfaceSourceCode(): void
+    public function testBuildInterfaceOfSourceCode(string $haystackFilePath, string $namespace, string $className, InterfaceMakerConfiguration $makerConfiguration): void
     {
-        $haystackFilePath = self::fixtureFilePath('make-interface/DummyInterface.php');
-        $namespace = 'App\Contracts';
-        $className = 'DummyInterface';
+        $haystackFilePath = self::fixtureFilePath($haystackFilePath);
 
-        $makerConfiguration = InterfaceMakerConfiguration::fromNamespace(
-            rootDir: __DIR__,
-            rootNamespace: self::rootNamespace(),
-            namespace: $namespace,
-            className: $className,
-        );
         $builder = new InterfaceBuilder();
 
         $phpFileDefinition = $builder->createPhpFileDefinition($makerConfiguration);
