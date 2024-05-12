150532267b4c7b7d8bb344a45c59acff7aa58872
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-05-12 12:08:24 +0200

    Test : add events to entities builder

---
+++ tests/fixtures/FixtureEntityWithoutEventsWithoutConstruct.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Atournayre\Bundle\MakerBundle\Tests\fixtures;
+
+use App\Repository\FixtureEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FixtureEntityRepository::class)]
+class FixtureEntityWithoutEventsWithoutConstruct
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
+
