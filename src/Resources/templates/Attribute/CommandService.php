4f00df57976aafb2ae932dccd653c2799633b6d9
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-05-18 10:23:10 +0200

    Attributes must have __construct in order to be instanced by reflection.

--- src/Resources/templates/Attribute/CommandService.php
+++ src/Resources/templates/Attribute/CommandService.php
@@ -6,5 +6,9 @@ namespace App\Attribute;
 #[\Attribute(\Attribute::TARGET_CLASS)]
 class CommandService
 {
-	public readonly string $serviceName;
+    public function __construct(
+        public readonly string $serviceName
+    )
+    {
+    }
 }
